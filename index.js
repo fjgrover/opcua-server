9043a4903ad6366e0456e5d0f24929754a7cb2a0
Forrest Grover <fjgrover@hotmail.com>
2020-06-28 17:35:01 -0400

    need to prepare for SSL

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require( 'express' );
 const app = express();
-const router = express().router();
 
 // start listener
 app.listen( 5000, () => {
     console.log( 'Server started' );
 });
 
-router.post('/', (req, res) => {
+app.post('/', (req, res) => {
     console.log( req );
 });
